2c24a8dd12f9100f1fb32accea8ccc3b571a1498
HariGadi143 <haridhonigadi0707@gmail.com>
2024-03-02 08:07:09 +0530

    worked on congrats card

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>
-        
+      <div className="mainContainer">
+        <h1 className="mainHeading">Congratulations</h1>
+        <div className="detailsContainer">
+          <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" className="congratsProfile" alt="congratsProfile"/>
+          <h1 className="profileName">Hari Gadi</h1>
+          <p className="collegeName">Dr.Lankapalli Bullaya Degree College, Visakhapatnam</p>
+          <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" alt="congratsWatch" className="congratsWatch"/>
+
+        </div>
+
       </div>
     </div>
   );
